be3c301861a0bd5f9b9a828c8d9d298008f6a506
akshay037 <akshayteltumbade69@gmail.com>
2022-10-11 10:25:54 +0530

    initiate repo

---
+++ Variable & datatypes.py
@@ -0,0 +1,7 @@
+characater_name = "John"
+characater_age = "23"
+
+print("There once was a man named " + characater_name +",")
+print("he was " + characater_age + " years old.")
+print("He really like the name " + characater_name + ",")
+print("but didn't like being " + characater_age + ".")
