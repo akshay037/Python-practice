a37869971bc1f8a99abb2be8a1895828c7fb8019
akshay037 <akshayteltumbade69@gmail.com>
2022-10-11 11:25:36 +0530

    Working with string and numbers

---
+++ String_and_number.py
@@ -0,0 +1,18 @@
+from math import *
+
+word = "my"
+print("hello "+ word +" bitches") 
+print(word.upper() + " buddies")
+
+
+
+print(-51.25531)
+print(51 + 15)
+print(51 - 21)
+print(51 * 21)
+print(51 / 2)
+print(3* (51 / 2))
+my_num = 37
+print(str(my_num)+" My favorite number")
+
+print(sqrt(36))
