fa6c7197aa8300fe6117f87d7717a03c3e44ffd3
akshay037 <akshayteltumbade69@gmail.com>
2023-03-28 12:45:32 +0530

    Completes Task 3

---
+++ Day_2.py
@@ -0,0 +1,6 @@
+# Task number 2 :- Python Program to Find the Square Root
+
+num = float(input('Enter a number: '))
+
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
