57667a8688d7320a9c22b5b1d137f1433f376af4
akshay037 <akshayteltumbade69@gmail.com>
2023-04-01 15:29:11 +0530

    Complete the

---
+++ Day_6.py
@@ -0,0 +1,17 @@
+# Task number 8 :- Python Program to Convert Kilometers to Miles
+
+kilo_meter = input("Enter value of Kilometer ")
+miles = 0.62137119
+
+result = int(kilo_meter) * miles
+
+print('%f is Total Miles'%result)
+
+# Extra work :- Python Program to Convert Miles to Kilometers
+
+kilo_meter = 1.609347
+miles = input("Enter value of Miles ")
+
+result = int(miles) * kilo_meter
+
+print('%f is Total Miles'%result)
