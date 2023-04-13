8cfb5015925c1f080499694af1a4bd36ab1f9dc3
akshay037 <akshayteltumbade69@gmail.com>
2023-04-13 14:47:59 +0530

    Complete task number 21

---
+++ Day_17.py
@@ -0,0 +1,7 @@
+# Task number 21 :- Python Program to Find the Sum of Natural Numbers
+
+num = int(input("Enter Number "))
+
+result = num*(num + 1)/2
+
+print("Total sum of natural numbers", result)
