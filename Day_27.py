342aa8dcbf5626773600cd3995900f48510b7a09
akshay037 <akshayteltumbade69@gmail.com>
2023-04-27 07:46:41 +0530

    Completes task number 31

---
+++ Day_27.py
@@ -0,0 +1,8 @@
+# Task number 31 :- Python Program to Display Calendar
+
+import calendar
+
+year = int(input("Enter year: "))
+month = int(input("Enter month: "))
+
+print(calendar.month(year, month))
