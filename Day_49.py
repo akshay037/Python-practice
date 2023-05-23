d310c4bda76f855b95fcd67ef89368a4510ebd80
akshay037 <akshayteltumbade69@gmail.com>
2023-05-23 08:16:11 +0530

    Complete task number 55

---
+++ Day_49.py
@@ -0,0 +1,19 @@
+# Task number 55 :- Python Program to Check If a List is Empty
+
+# Using Boolean operation
+
+my_list = []
+if not my_list:
+    print("the list is empty")
+
+# Using len()
+
+my_list = []
+if not len(my_list):
+    print("the list is empty")
+
+# Comparing with []
+
+my_list = []
+if my_list == []:
+    print("The list is empty")
