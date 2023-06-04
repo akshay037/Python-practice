b87e6b86543203a474346080f0380badee234fd5
akshay037 <akshayteltumbade69@gmail.com>
2023-06-04 22:11:42 +0530

    Completes task number 65

---
+++ Day_58.py
@@ -0,0 +1,12 @@
+# Task number 65 :- Python Program to Get a Substring of a String
+
+my_string = input("Enter the string ")
+
+# prints "love"
+print(my_string[2:6])
+
+# prints "love python."
+print(my_string[2:])
+
+# prints "I love python"
+print(my_string[:-1])
