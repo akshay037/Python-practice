497b593b69d939fc8282b3e3091a880056348f44
akshay037 <akshayteltumbade69@gmail.com>
2023-06-26 07:55:47 +0530

    Complete task number 74

---
+++ Day_67/Day_67.py
@@ -0,0 +1,7 @@
+# Task number 74 :- Python Program to Extract Extension From the File Name
+
+import os
+file_details = os.path.splitext('D:\Python programming\Day_67\Sample.txt')
+print(file_details)
+print(file_details[1])
+
