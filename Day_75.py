fe075985beb032fb0f5314fe008c80d1a1472191
akshay037 <akshayteltumbade69@gmail.com>
2023-07-05 06:58:45 +0530

    Complete task number 81

---
+++ Day_75.py
@@ -0,0 +1,11 @@
+# Task number 81 :- Python Program to Return Multiple Values From a Function
+
+def name():
+    return "John","Armin"
+
+# print the tuple with the returned values
+print(name())
+
+# get the individual items
+name_1, name_2 = name()
+print(name_1, name_2)
